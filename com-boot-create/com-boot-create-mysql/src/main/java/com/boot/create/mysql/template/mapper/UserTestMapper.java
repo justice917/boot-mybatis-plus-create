7e72f2ef2665893549f6d099f2ae7c29c3a4b8f0
taojin <123@163.com>
2021-03-25 15:31:21 +0800

    ~

---
+++ com-boot-create/com-boot-create-mysql/src/main/java/com/boot/create/mysql/template/mapper/UserTestMapper.java
@@ -0,0 +1,16 @@
+package com.boot.create.mysql.template.mapper;
+
+import com.boot.create.mysql.template.entity.UserTest;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户测试 Mapper 接口
+ * </p>
+ *
+ * @author taojin
+ * @since 2021-03-25
+ */
+public interface UserTestMapper extends BaseMapper<UserTest> {
+
+}
